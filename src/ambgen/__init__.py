1a468840923aaa68f8585d28484a14a47851ebfe
Timothy Click <tclick@okstate.edu>
2021-01-13 08:05:02 -0600

    Added logging configuration function
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- src/ambgen/__init__.py
+++ src/ambgen/__init__.py
@@ -13,4 +13,64 @@
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
 
-__version__ = "0.1.0"
+import logging
+from typing import Dict
+
+logger: logging.Logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+__version__: str = "0.1.0"
+
+
+def create_logging_dict(logfile: str) -> Dict:
+    """Configure the logger.
+
+    Parameters
+    ----------
+    logfile : str
+        Filename for log output.
+
+    Returns
+    -------
+    dict
+        Configuration data for logging.
+
+    Raises
+    -------
+    ValueError
+        If a filename is not defined.
+    """
+    if not logfile:
+        raise ValueError("Filename not defined.")
+
+    logger_dict = dict(
+        version=1,
+        disable_existing_loggers=False,  # this fixes the problem
+        formatters=dict(
+            standard={
+                "class": "logging.Formatter",
+                "format": "%(name)-12s %(levelname)-8s %(message)s",
+            },
+            detailed={
+                "class": "logging.Formatter",
+                "format": ("%(asctime)s %(name)-15s %(levelname)-8s " "%(message)s"),
+                "datefmt": "%m-%d-%y %H:%M",
+            },
+        ),
+        handlers=dict(
+            console={
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "standard",
+            },
+            file={
+                "class": "logging.FileHandler",
+                "filename": logfile,
+                "level": "INFO",
+                "mode": "w",
+                "formatter": "detailed",
+            },
+        ),
+        root=dict(level="INFO", handlers=["console", "file"]),
+    )
+    return logger_dict
